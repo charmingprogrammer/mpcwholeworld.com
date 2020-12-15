e65fe58c05f974c04092c21c16de4aaf7be547eb
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2020-12-15 12:43:38 +0530

    Add files via upload

---
+++ colortictacto.js
@@ -0,0 +1,73 @@
+
+function showName() {
+    var player1name = document.redForm.player1.value;
+    var player2name = document.greenForm.player2.value;
+    document.getElementById('redPlayer').innerHTML = player1name;
+    document.getElementById('greenPlayer').innerHTML = player2name;
+
+}
+function block1a() {
+    document.getElementById('div1').style.background = "red";
+}
+
+function block1b() {
+    document.getElementById('div1').style.background = "#33ff00";
+}
+function block2a() {
+    document.getElementById('div2').style.background = "red";
+}
+
+function block2b() {
+    document.getElementById('div2').style.background = "#33ff00";
+}
+function block3a() {
+    document.getElementById('div3').style.background = "red";
+}
+
+function block3b() {
+    document.getElementById('div3').style.background = "#33ff00";
+}
+function block4a() {
+    document.getElementById('div4').style.background = "red";
+}
+
+function block4b() {
+    document.getElementById('div4').style.background = "#33ff00";
+}
+function block5a() {
+    document.getElementById('div5').style.background = "red";
+}
+
+function block5b() {
+    document.getElementById('div5').style.background = "#33ff00";
+}
+function block6a() {
+    document.getElementById('div6').style.background = "red";
+}
+
+function block6b() {
+    document.getElementById('div6').style.background = "#33ff00";
+}
+function block7a() {
+    document.getElementById('div7').style.background = "red";
+}
+
+function block7b() {
+    document.getElementById('div7').style.background = "#33ff00";
+}
+function block8a() {
+    document.getElementById('div8').style.background = "red";
+}
+
+function block8b() {
+    document.getElementById('div8').style.background = "#33ff00";
+}
+function block9a() {
+    document.getElementById('div9').style.background = "red";
+}
+
+function block9b() {
+    document.getElementById('div9').style.background = "#33ff00";
+
+}
+
