a88dc7a22034a2ab5983e2a14ea3caac13dcda63
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-04-15 12:47:38 +0530

    Add files via upload

---
+++ moneymoj.js
@@ -0,0 +1,154 @@
+
+//function for home ...
+document.getElementById('home').style.color = "red";
+function home() {
+  // body...
+  document.querySelector('*').scrollTo(0,0);
+document.getElementById('movies').style.color = "";
+document.getElementById('home').style.color = "red";
+document.getElementById('music').style.color = "";
+document.getElementById('news').style.color = "";
+document.getElementById('story').style.color = "";
+document.getElementById('video1').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/prQ885_-Zwo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video2').innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/QmAkDLomKGw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+document.getElementById('video3').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/lg2i2oqnoy0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video4').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/ZmE6TN9bYQg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+
+document.getElementById('video5').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/TBZUSRwJgZs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video6').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/sCbbMZ-q4-I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video7').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/PJD7YnBZ5kw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video8').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/HO7CRp44s10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video9').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/rq5UZOonaes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video10').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/sRwcFL4BNfY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+}
+function movies() {
+  // body...
+document.querySelector('*').scrollTo(0,0);
+document.getElementById('video1').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/PWSyRtyPYXM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video2').innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/QmAkDLomKGw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+document.getElementById('video3').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/lg2i2oqnoy0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video4').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/ZmE6TN9bYQg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+
+document.getElementById('video5').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/TBZUSRwJgZs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video6').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Jxy2iCbNyaQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video7').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/tSNkcU5zYYY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+document.getElementById('video8').innerHTML ='<iframe width="100%" height="100" src="https://www.youtube.com/embed/b4BV7T_UHUA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video9').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/gxBERAhEU7w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video10').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/SXjKUEC-wOM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('movies').style.color = "red";
+document.getElementById('home').style.color = "";
+document.getElementById('music').style.color = "";
+document.getElementById('news').style.color = "";
+document.getElementById('story').style.color = "";
+
+}
+function music() {
+  // body...
+document.querySelector('*').scrollTo(0,0);
+document.getElementById('video1').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/VOLKJJvfAbg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video2').innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/hUQ6kVhoM88?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video3').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/hGf8rOwFzvo?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video4').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/PJD7YnBZ5kw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video5').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/HO7CRp44s10" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video6').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/FxAG_11PzCk?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video7').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/tQL-gx7PeBc?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+document.getElementById('video8').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/z-diRlyLGzo?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video9').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/SBWYGGDYmhg?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video10').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/tOXMlseYc7E?list=RDCLAK5uy_kCO40RkB9MzrBJF8p6uDcMP7tq2SUmrOE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('movies').style.color = "";
+document.getElementById('home').style.color = "";
+document.getElementById('music').style.color = "red";
+document.getElementById('news').style.color = "";
+document.getElementById('story').style.color = "";
+
+}
+function news() {
+  // body...
+document.querySelector('*').scrollTo(0,0);
+document.getElementById('video1').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/DZCElJyPfG0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video2').innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/kw-_ZTRv8Uw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+document.getElementById('video3').innerHTML='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/614UTCYPlVg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video4').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/6E7_A_eY8cg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video5').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/MN8p-Vrn6G0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video6').innerHTML ='<iframe width="100%" height="100%" src="https://www.youtube.com/embed/614UTCYPlVg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+
+document.getElementById('video7').innerHTML =''
+document.getElementById('video8').innerHTML =''
+
+document.getElementById('video9').innerHTML =''
+
+document.getElementById('video10').innerHTML =''
+
+document.getElementById('movies').style.color = "";
+document.getElementById('home').style.color = "";
+document.getElementById('music').style.color = "";
+document.getElementById('news').style.color = "red";
+document.getElementById('story').style.color = "";
+
+}
+function story() {
+  // body...
+document.querySelector('*').scrollTo(0,0);
+document.querySelector('.mainContainer').innerHTML='<iframe width="100%" height="900px" src="https://www.mxtakatak.com/" ></iframe>';
+document.getElementById('movies').style.color = "";
+document.getElementById('home').style.color = "";
+document.getElementById('music').style.color = "";
+document.getElementById('news').style.color = "";
+document.getElementById('story').style.color = "red";
+
+}
+
+function closeAd() {
+  document.getElementById("adsPop").style.transform = 'scale(0)';
+}
+
+setInterval(function() {
+document.getElementById("adsPop").style.transform = 'scale(1)';
+},60000)
+
+var moneyAdd = setInterval (function(){
+ var getvalue =  document.getElementById('money').value;
+ var parsingValue  = parseInt(getvalue);
+ var addvalue = parsingValue+1+"second";
+ var saveMoney = document.getElementById('money').value = addvalue;
+localStorage.setItem('money',"saveMoney")
+},1000)
+
+
+var getValue = JSON.parse(localStorage.getItem('money'));
+document.getElementById('money').value = getValue;
+
+function loader() {
+  document.getElementById("loader").style.
+  transform = "scale(0)";
+}
